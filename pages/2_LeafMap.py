5ee3011bb1d318c40c0f57d4d1eabfbb084ceafe
springonions-87 <wenchenisa@gmail.com>
2023-11-24 16:21:44 +0100

    new files

---
+++ pages/2_LeafMap.py
@@ -0,0 +1,12 @@
+# import sys
+import leafmap.foliumap as leafmap
+
+# sys.path
+
+raster_file = '/Users/wenyuc/Desktop/UT/data/raster/fuzzy_complete_3857.tif'
+
+m = leafmap.Map()
+m.add_basemap()
+m.add_raster(raster_file, cmap="viridis", layer_name="Raster Layer")
+
+m.to_streamlit()
